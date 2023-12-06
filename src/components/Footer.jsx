f6c170117bc1697130b96828d75b22ef000a0c50
William Casey <williamcaseylucas@gmail.com>
2023-12-06 17:00:49 -0500

    made hover for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import { TiSocialFacebookCircular } from "react-icons/ti"; // facebook
 import { MdEmail } from "react-icons/md"; // email
 import { Link } from "react-router-dom";
 
-const Footer = ({page}) => {
+const Footer = () => {
   return (
-    <div className="w-full h-60 bg-blue-800 text-white font-semibold">
+    <div className="w-full h-60 bg-blue-800 text-white hover:text-slate-100 font-semibold">
       <div className="flex max-w-screen-xl mx-auto">
         {/* Site */}
         <div className="w-1/3 p-4 m-4">
           <h3 className="text-2xl font-bold mb-4">Site</h3>
           <ul>
             <li>
-              <a href="">About</a>
+              <a className="hover:text-slate-300 cursor-pointer">About</a>
             </li>
             <li>
-              <a href="">Help</a>
+              <a className="hover:text-slate-300 cursor-pointer">Help</a>
             </li>
           </ul>
         </div>
@@ -26,25 +26,31 @@ const Footer = ({page}) => {
           <ul>
             <li>
               <Link
-              className={`hover:text-slate-300 ${
-                page === "terms" && "underline underline-offset-8"
-              }`}
-              to={"/terms"}
+                className="hover:text-slate-300 cursor-pointer"
+                to={"/terms"}
               >
-              Terms & Conditions
+                Terms & Conditions
               </Link>
             </li>
             <li>
-              <a href="">Privacy Policy</a>
+              <a className="hover:text-slate-300 cursor-pointer">
+                Privacy Policy
+              </a>
             </li>
             <li>
-              <a href="">Copyright Compliance Policy</a>
+              <a className="hover:text-slate-300 cursor-pointer">
+                Copyright Compliance Policy
+              </a>
             </li>
             <li>
-              <a href="">Notice at Collection</a>
+              <a className="hover:text-slate-300 cursor-pointer">
+                Notice at Collection
+              </a>
             </li>
             <li>
-              <a href="">Do Not Tell or Share My Personal Information</a>
+              <a className="hover:text-slate-300 cursor-pointer">
+                Do Not Tell or Share My Personal Information
+              </a>
             </li>
           </ul>
         </div>
@@ -53,10 +59,22 @@ const Footer = ({page}) => {
         <div className="w-1/3 p-4 m-4 flex flex-col">
           <h3 className="text-2xl font-bold mb-4">Contact Us</h3>
           <div className="flex gap-4">
-            <TiSocialTwitter className="cursor-pointer" size={24} />
-            <SlSocialInstagram className="cursor-pointer" size={24} />
-            <TiSocialFacebookCircular className="cursor-pointer" size={24} />
-            <MdEmail className="cursor-pointer" size={24} />
+            <TiSocialTwitter
+              className="hover:text-slate-300 cursor-pointer"
+              size={24}
+            />
+            <SlSocialInstagram
+              className="hover:text-slate-300 cursor-pointer"
+              size={24}
+            />
+            <TiSocialFacebookCircular
+              className="hover:text-slate-300 cursor-pointer"
+              size={24}
+            />
+            <MdEmail
+              className="hover:text-slate-300 cursor-pointer"
+              size={24}
+            />
           </div>
           <span className="flex-grow flex flex-col-reverse">
             &copy; {new Date().getFullYear()} LMC 3403, Inc. All Rights Reserved
