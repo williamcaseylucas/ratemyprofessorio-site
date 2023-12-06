bb2e33fa90c512979a0b1e51c931c3d5cbf1200e
William Casey <williamcaseylucas@gmail.com>
2023-12-06 14:01:51 -0500

    changed stills to screenshots

--- src/components/Stillshots.jsx
+++ src/components/Stillshots.jsx
@@ -3,7 +3,7 @@ const Stillshots = ({ image1, image2 }) => {
   console.log("image2", image2);
   return (
     <div className="w-full">
-      <h1 className="text-6xl text-center mb-6">Stills</h1>
+      <h1 className="text-6xl text-center mb-6">Screenshots</h1>
       <div className="p-5 gap-5 flex justify-between">
         <div className="" id="left">
           <img className="rounded-xl" src={image1} alt="" />
